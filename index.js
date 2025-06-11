e862fc3121f1568fb786114753f31d65cdb5bacc
jc16g22 <jc16g22@soton.ac.uk>
2025-06-11 11:07:47 +0100

    Debug changes for deployment

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", authRoutes);
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
 
-const PORT = process.env.PORT || 4000;
+
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
